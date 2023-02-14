840b88a460fbbcfcade7fd16c7179ae0bb9078bf
wh4 <75698073+bohdan-256@users.noreply.github.com>
2023-02-14 04:09:41 +0100

    Update download.py

--- download.py
+++ download.py
@@ -1,8 +1,26 @@
-import wget
 import os
-
-base_url = "https://github.com/bohdan-256/test_stie/blob/main/JopaForum.html" 
-print("start download...")
-wget.download(url = base_url , out = "test.html" )
-    
-
+import time
+def dota():
+    print("start..")
+    os.system("start steam://rungameid/570%22")
+    time.sleep(10) 
+def csgo():
+    print("start..")
+    os.system("start steam://rungameid/730%22")
+    time.sleep(10) 
+def dst():
+    print("start..")
+    os.system("start steam://rungameid/322330")
+    time.sleep(10) 
+def gmod():
+    print("start..")
+    os.system("start steam://rungameid/252490%22")
+    time.sleep(10) 
+def rust():
+    print("start..")
+    os.system("start steam://rungameid/252490%22")
+    time.sleep(10) 
+def snd():
+    print("start..")
+    os.system("start steam://rungameid/629520%22")
+    time.sleep(10) 
